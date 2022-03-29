5d65907b3e8c7ff53ca773007c168c23d5f75d40
David Souza <dabcs@poli.ufrj.br>
2022-03-29 15:06:11 -0300

    mudança do projeto para ./app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import rescue from 'express-rescue';
+import ordersRouter from './routes/orders.routes';
+import productsRouter from './routes/products.routes';
+import userRouter from './routes/users.routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(rescue(productsRouter));
+app.use(rescue(userRouter));
+app.use(rescue(ordersRouter));
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-import rescue from 'express-rescue';
 import app from './app';
-import ordersRouter from './routes/orders.routes';
-import productsRouter from './routes/products.routes';
-import userRouter from './routes/users.routes';
 
 const PORT = 3000;
-
-app.use(rescue(productsRouter));
-app.use(rescue(userRouter));
-app.use(rescue(ordersRouter));
-
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
 ));
